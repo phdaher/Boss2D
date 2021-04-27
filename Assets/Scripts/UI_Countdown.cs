89203381e865e90f35f60303a37450a3ca3f4e4f
Pedro Daher <pedrohenriquedaher@icloud.com>
2021-04-26 21:03:05 -0300

    Final Version

---
+++ Assets/Scripts/UI_Countdown.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class UI_Countdown : MonoBehaviour
+{
+   Text textComp;
+    GameManager gm;
+
+
+   public float timeRemaining = 5;
+   void Start()
+   {
+       textComp = GetComponent<Text>();
+       gm = GameManager.instance;     
+   }
+   
+   void Update()
+   {
+       if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else 
+        {
+            timeRemaining = 5;
+            gm.NextLevel();
+        }
+       textComp.text = $"Desativa em: {Math.Truncate(timeRemaining)} s";
+   }
+}
---
+++ Assets/Scripts/UI_Record.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UI_Record : MonoBehaviour
+{
+    Text textComp;
+    GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       textComp = GetComponent<Text>();   
+       gm = GameManager.instance;     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       textComp.text = $"Record: Level {gm.recordLevel}";        
+    }
+}
